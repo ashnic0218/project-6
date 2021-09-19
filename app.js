d6533bf57bda07e41fef24870df2705ef034ba98
Kris <v.kristofar@gmail.com>
2021-09-19 22:36:09 +0300

    added sauce model

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+// MONGODB PW: pKgF9ci7ofKBIkaq
+// MONGODB CONNECTION: mongodb+srv://kris-v:<password>@cluster0.8bnnq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Sauce = require('./models/sauce');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://kris-v:pKgF9ci7ofKBIkaq@cluster0.8bnnq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,27 +25,80 @@ app.use((req, res, next) => {
   next();
 });
 
+
 app.use(bodyParser.json());
 
-app.post('/api/auth/signup', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'User created successfully!'
+
+// app.post('/api/auth/signup', (req, res, next) => {
+//   console.log(req.body);
+//   res.status(201).json({
+//     message: 'User created successfully!'
+//   });
+// });
+
+
+// const sauceSchema = mongoose.Schema({  
+//   userId: { type: String, required: true },
+//   name: { type: String, required: true },
+//   manufacturer: { type: String, required: true },
+//   description: { type: String, required: true },
+//   mainPepper: { type: String, required: true },
+//   imageUrl: { type: String, required: true },
+//   heat: { type: Number, required: true },
+//   likes: { type: Number, required: true },
+//   dislikes: { type: Number, required: true },
+//   usersLiked: { type: Array, required: true },
+//   usersDisliked: { type: Array, required: true }
+// });
+
+app.post('/api/sauces', (req, res, next) => {
+  const sauce = new Sauce({
+    userId: req.body.userId,
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    mainPepper: req.body.mainPepper,
+    imageUrl: req.body.imageUrl,
+    heat: req.body.heat,
+    likes: req.body.likes,
+    dislikes: req.body.dislikes,
+    usersLiked: req.body.usersLiked,
+    usersDisliked: req.body.usersDisliked
   });
+  sauce.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      })
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
 });
 
-app.use('/api/stuff', (req, res, next) => {
+
+app.use('/api/sauces', (req, res, next) => {
   const stuff = [
     {
       _id: 'oeihfzeoi',
-      title: 'My first thing',
-      description: 'All of the info about my first thing',
-      imageUrl: '',
-      price: 4900,
-      userId: 'qsomihvqios',
+      userId: 'req.body.userId',
+      name: 'req.body.name',
+      manufacturer: 'req.body.manufacturer',
+      description: 'req.body.description',
+      mainPepper: 'req.body.mainPepper',
+      imageUrl: 'req.body.imageUrl',
+      heat: 'req.body.heat',
+      likes: 'req.body.likes',
+      dislikes: 'req.body.dislikes',
+      usersLiked: 'req.body.usersLiked',
+      usersDisliked: 'req.body.usersDisliked'
     },
     {
-      _id: 'oeihfzeomoihi',
+      _id: 'someRandomId',
       title: 'My second thing',
       description: 'All of the info about my second thing',
       imageUrl: '',
