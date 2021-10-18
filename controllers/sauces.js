05deddbdef0acf3d35e0dc544865b85b9101aefd
Kris <v.kristofar@gmail.com>
2021-10-18 09:12:05 +0300

    added rating controller

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,5 +1,6 @@
 const Sauce = require('../models/sauce');
 const fs = require('fs');
+const { findOne } = require('../models/sauce');
 
 exports.createSauce = (req, res, next) => {
     const url = req.protocol + '://' + req.get('host');
@@ -55,17 +56,17 @@ exports.modifySauce = (req, res, next) => {
         req.body.sauce = JSON.parse(req.body.sauce);
         sauce = {
             _id: req.params.id,
-            userId: req.body.sauce.userId,
+            // userId: req.body.sauce.userId,
             name: req.body.sauce.name,
             manufacturer: req.body.sauce.manufacturer,
             description: req.body.sauce.description,
             mainPepper: req.body.sauce.mainPepper,
             imageUrl: url + '/images/' + req.file.filename,
             heat: req.body.sauce.heat,
-            likes: req.body.sauce.likes,
-            dislikes: req.body.sauce.dislikes,
-            usersLiked: req.body.sauce.usersLiked,
-            usersDisliked: req.body.sauce.usersDisliked
+            // likes: req.body.sauce.likes,
+            // dislikes: req.body.sauce.dislikes,
+            // usersLiked: req.body.sauce.usersLiked,
+            // usersDisliked: req.body.sauce.usersDisliked
         };
     } else {
         sauce = {
@@ -77,16 +78,16 @@ exports.modifySauce = (req, res, next) => {
             mainPepper: req.body.mainPepper,
             imageUrl: req.body.imageUrl,
             heat: req.body.heat,
-            likes: req.body.likes,
-            dislikes: req.body.dislikes,
-            usersLiked: req.body.usersLiked,
-            usersDisliked: req.body.usersDisliked
+            // likes: req.body.likes,
+            // dislikes: req.body.dislikes,
+            // usersLiked: req.body.usersLiked,
+            // usersDisliked: req.body.usersDisliked
         };
     }
     Sauce.updateOne({_id: req.params.id}, sauce).then(
         () => {
         res.status(201).json({
-            message: 'Thing updated successfully!'
+            message: 'Sauce updated successfully!'
         });
         }
     ).catch(
@@ -134,3 +135,63 @@ exports.getAllSauces = (req, res, next) => {
         }
     );
 };
+
+exports.getRating = (req, res, next) => {
+    // find the sauce using Sauce.findOne()
+    // use a conditional that lets me know which thumb has been clicked using req.body.like that will give a number (1, -1, 0)
+    // the conditional has 4 steps
+    // let updatedLikes = new Sauce({ _id: req.params._id });
+    console.log('body', req.body)
+    console.log('body', req.body.usersLiked)
+        Sauce.findOne({_id: req.params.id}).then(
+            (updatedLikes) => {
+                if (req.body.like === 1) {
+                    console.log('I guess it is a like')
+                    console.log(req.body.likes)
+                    updatedLikes = {
+                        _id: req.params.id,
+                        userId: req.body.userId,
+                        name: req.body.name,
+                        manufacturer: req.body.manufacturer,
+                        description: req.body.description,
+                        mainPepper: req.body.mainPepper,
+                        imageUrl: req.body.imageUrl,
+                        heat: req.body.heat,
+                        likes: req.body.likes + 1,
+                        dislikes: req.body.dislikes,
+                        usersLiked: req.body.usersLiked,
+                        usersDisliked: req.body.usersDisliked,
+                    } // object I am using to update the record
+                } else if (req.body.like === -1) {
+                    console.log('They probably did not like it')
+                    updatedLikes = {
+                        _id: req.params.id,
+                        userId: req.body.userId,
+                        name: req.body.name,
+                        manufacturer: req.body.manufacturer,
+                        description: req.body.description,
+                        mainPepper: req.body.mainPepper,
+                        imageUrl: req.body.imageUrl,
+                        heat: req.body.heat,
+                        likes: req.body.likes,
+                        dislikes: req.body.dislikes + 1,
+                        usersLiked: req.body.usersLiked,
+                        usersDisliked: req.body.usersDisliked,
+                    }
+                } 
+
+            Sauce.updateOne({_id: req.params.id}, updatedLikes)
+            .then(() => {
+                res.status(201).json({
+                    message: 'Great Sauce!'
+            });
+            })
+            .catch(
+                (error) => {
+                res.status(400).json({
+                    error: error
+                });
+            }
+        );
+    }
+)}
--- routes/sauces.js
+++ routes/sauces.js
@@ -11,6 +11,7 @@ router.get('/', auth, saucesCtrl.getAllSauces);
 router.post('/', auth, multer, saucesCtrl.createSauce);
 router.get('/:id', auth, saucesCtrl.getOneSauce);
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.post('/:id/like', auth, saucesCtrl.getRating);
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
 
 module.exports = router;
